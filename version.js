3b3813b45226468b0d8a3c51ad66a776c91c437d
Ben Merckx <ben@codeurs.be>
2020-10-28 15:11:59 +0100

    Cleanup version

--- version.js
+++ version.js
@@ -1,12 +1,3 @@
 const fs = require("fs");
 const version = JSON.parse(fs.readFileSync(".haxerc")).version;
-fs.writeFileSync(
-  "build/js/package.json",
-  JSON.stringify({
-    type: "module",
-    name: "helder.std",
-    version: version,
-    repository: "https://github.com/helder/std",
-    license: "MIT",
-  }, null, '  ')
-);
+console.log(version);
