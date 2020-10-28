f3a7366ad34409ab1784fb488d41f48f03c7c7cf
Ben Merckx <ben@codeurs.be>
2020-10-28 14:13:21 +0100

    Tweaks

--- src/helder/Std.hx
+++ src/helder/Std.hx
@@ -1,5 +1,6 @@
 package helder;
 
+#if macro
 import haxe.macro.Type;
 import haxe.macro.Context;
 
@@ -7,54 +8,60 @@ using haxe.macro.TypeTools;
 
 class Std {
   static final stdPaths = ['haxe', 'sys'];
+  static final root = [
+    'Any', 'Array', 'Class', 'Date', 'DateTools', 'EReg', 'Enum', 'EnumValue', 
+    'IntIterator', 'Lambda', 'List', 'Map', 'Math', 'Expr', 'Reflect', 'Std', 
+    'StdTypes', 'String', 'StringBuf', 'StringTools', 'Sys', 'Type', 'UInt', 
+    'UnicodeString', 'Xml'
+  ];
+	static final exclude = ['haxe.macro', 'php.Boot', 'haxe.Exception'];
 
-  public static function use() {
-    #if php
-    excludeAsNative(['php'].concat(stdPaths), (pack, type) -> {
-      final native = ['helder', 'std'].concat(pack.split('.'))
-        .concat([type.name])
-        .join('\\');
-      type.meta.add(':native', [macro $v{native}], type.pos);
-    });
-    #elseif genes
-    excludeAsNative(['js', 'genes'].concat(stdPaths), (pack, type) -> {
-      final path = type.module.split('.');
-      final name = type.name;
-      final from = ['helder.std'].concat(path).join('/');
-      type.meta.add(':jsRequire', [macro $v{from}, macro $v{name}], type.pos);
-    });
-    #end
-  }
+	public static function use() {
+		if (Context.defined('php'))
+			excludeAsNative(['php'].concat(stdPaths), (pack, type) -> {
+				final path = if (pack == '') [] else pack.split('.');
+				final name = if (pack == '' && type.name == 'Array') 'Array_hx' else type.name;
+				final native = ['helder', 'std'].concat(path).concat([name]).join('\\');
+				type.meta.add(':native', [macro $v{native}], type.pos);
+			});
+		else if (Context.defined('genes'))
+			excludeAsNative(['js', 'genes'].concat(stdPaths), (pack, type) -> {
+				final path = type.module.split('.');
+				final name = type.name;
+				final from = ['helder.std'].concat(path).join('/');
+				type.meta.add(':jsRequire', [macro $v{from}, macro $v{name}], type.pos);
+			});
+	}
 
-  static function includesPack(packs: Array<String>, pack: String) {
-    for (p in packs)
-      if (StringTools.startsWith(pack, p + '.'))
-        return true;
-    return false;
-  }
+	static function includesPack(packs:Array<String>, pack:String) {
+		for (p in packs)
+			if (pack == p || StringTools.startsWith(pack, p + '.'))
+				return true;
+		return false;
+	}
 
-  static function excludeAsNative(packs: Array<String>,
-      makeNative: (pack: String, type: BaseType) -> Void) {
-    Context.onGenerate(function(types) {
-      for (type in types) {
-        switch type {
-          case TInst((_.get() : BaseType) => base, _) |
-            TEnum((_.get() : BaseType) => base, _):
-            final pack = base.pack.join('.');
-            if (pack != '' && !includesPack(packs, pack))
-              continue;
-            if (base.isExtern)
-              continue;
-            final meta = base.meta;
-            if (!meta.has(":nativeGen")) {
-              meta.add(":hxGen", [], base.pos);
-              makeNative(pack, base);
-            }
-            base.exclude();
-          default:
-            continue;
-        }
-      }
-    }, false);
-  }
+	static function excludeAsNative(packs:Array<String>, makeNative:(pack:String, type:BaseType) -> Void) {
+		Context.onGenerate(function(types) {
+			for (type in types) {
+				switch type {
+					case TInst((_.get() : BaseType) => base, _) | TEnum((_.get() : BaseType) => base, _):
+						final pack = base.pack.join('.');
+						if (root.indexOf(pack) == -1 && !includesPack(packs, pack))
+							continue;
+						if (base.isExtern)
+							continue;
+						final meta = base.meta;
+						if (includesPack(exclude, base.module))
+							continue;
+						if (!meta.has(":nativeGen")) {
+							meta.add(":hxGen", [], base.pos);
+							makeNative(pack, base);
+						}
+						base.exclude();
+					default:
+				}
+			}
+		}, false);
+	}
 }
+#end
