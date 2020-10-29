cd61f5acae5b01e44501b9ba87675d11fc158e5f
Ben Merckx <ben@codeurs.be>
2020-10-29 11:05:54 +0100

    Use build meta for tags

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const libVersion = JSON.parse(fs.readFileSync("haxelib.json")).version;
 const haxeVersion = JSON.parse(fs.readFileSync(".haxerc")).version;
-console.log(`${haxeVersion}-${libVersion}`);
+console.log(`${haxeVersion}+${libVersion}`);
